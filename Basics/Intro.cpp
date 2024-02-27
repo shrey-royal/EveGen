bcea8c204f8c18278a39266fd626a8ae6e6db49f
Shrey K <srkkadia@gmail.com>
2024-02-27 09:27:05 +0530

    Introdution to cpp and print/scan

---
+++ Basics/Intro.cpp
@@ -0,0 +1,42 @@
+/*
+CPP/C++: C PLUS PLUS
+C++ is a general purpose programming language and widely used nowadays for competitive programming and in dsa due to it's lower compile time and runtime
+
+> It is a semi Object Oriented Programming Language.
+> header file - iostream (stream is a continuous flow of data)
+
+*/
+
+#include<iostream>  //header file - input output stream
+using namespace std;    //namespace created by c++
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
+
+/*
+iostream -> input output stream (istream + ostream)
+istream -> input stream (cin)
+ostream -> output stream (cout)
+
+cout -> console output stream -> used to print the output
+> cout uses insertion operator (<<) to print the output
+
+cin -> console input stream -> used to take input from the user
+> cin uses extraction operator (>>) to take input from the user
+
+Difference between void main() and int main():
+
+> void main() is not a standard way to write the main function
+> int main() is a standard way to write the main function
+
+> void main() is not returning any value
+> int main() is returning 0 if program is successfully executed
+
+> void main() is not taking any parameter
+> int main() is taking two parameters (int argc, char* argv[])
+
+
+*/
