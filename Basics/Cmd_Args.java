ba5809370d001741f2e18b502fbb87c9fbba2541
Shrey K <srkkadia@gmail.com>
2024-06-07 17:01:20 +0530

    Basics of Java and Command Line Arguments

---
+++ Basics/Cmd_Args.java
@@ -0,0 +1,8 @@
+public class Cmd_Args {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+            // System.out.println(args[i].hashCode());
+        }
+    }
+}
