6b25abce2a2ce603f626ce8826ef7a854d18a0cd
Shrey K <srkkadia@gmail.com>
2024-06-16 16:02:31 +0530

    Java Array (1d, 2d)

---
+++ Arrays/OneArray.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class OneArray {
+	public static void main(String[] args) {
+		/*
+		// int arr[] = new int[10];
+		//Declaration of array
+		int arr[];
+		//Instantiation of array
+		arr = new int[10];
+		/ *
+		int []a1[] = new int[3][3];
+		int[] []a1;
+		int a1[][];
+		int[][] a1;
+		int [][]a1;
+		* /
+		
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print("Enter arr[" + i + "]: ");
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+		*/
+		/*
+		int []a1[] = new int[3][3];
+		
+		//System.out.println(Math.random());
+		
+		for(int i=0; i<a1.length; i++) {
+			for(int j=0; j<a1[0].length; j++) {
+				a1[i][j] = (int)(Math.random() * 100);
+			}
+		}
+		
+		for(int i=0; i<a1.length; i++) {
+			for(int j=0; j<a1[0].length; j++) {
+				System.out.print(a1[i][j] + ", ");
+				//System.out.printf("%2d, ", a1[i][j]);
+			}
+			System.out.println("\b\b ");
+		}
+		*/
+		
+		String[] arr = new String[5];
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<arr.length; i++) {
+			System.out.print("Enter a string: ");
+			arr[i] = sc.nextLine();
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+	}
+}
--- Basics/Test/CS_T22.java
+++ Basics/Test/CS_T22.java
@@ -4,8 +4,10 @@ public class CS_T22 {
 	public static void main(String[] args) {
 		Scanner sc =  new Scanner(System.in);
 		
+		
 		System.out.print("Input number of rows : ");
 		int n = sc.nextInt();
+		
 		
 		for(int i=0; i<n; i++) {
 			for(int k=0; k<n-i; k++) {
